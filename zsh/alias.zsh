bb372565d66cfb8340c77ea95a8d25e7944e3faa
soyccan <soyccan@gmail.com>
2019-12-05 15:56:05 +0800

    alias update

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,51 +1,31 @@
-_docker_alias() {
-    alias dat='docker attach'
-    alias dcon='docker container'
-    alias dim='docker image'
-    alias dps='docker ps'
-    alias dpsa='docker ps -a'
-    alias dsta='docker start -ai'
-    alias dsto='docker stop'
-    alias dex='docker exec -it'
-}
+# common aliases
+alias cp='cp -irv' # --interactive --recursive --verbose
+alias df='df -h' # --human-readable
+alias du='du -h'  # --human-readable --apparent-size(-A) ; -A connot be used in macOS
+alias free='free -h' # --human
+alias less='less -R' # --RAW-CONTROL-CHARS ; color only
+alias ln='ln -s' # --symbolic'
+alias mv='mv -i' # --interactive
+alias rm='rm -i' # --interactive=always
+alias tar='tar -v' # --verbose'
+alias wget='wget --timestamping -c' # --continue
+alias hd='hexdump -C' # hex+ascii
 
-if [ "$(uname)" = 'Linux' ]; then
-    alias cp="cp --interactive --recursive --verbose"
-    alias df="df --human-readable"
-    alias du="du --human-readable --apparent-size"
-    alias free="free --human"
-    alias gcc="gcc -Wall -Wextra -Wconversion -std=c18   -g -Dsoyccan"
-    alias g++="g++ -Wall -Wextra -Wconversion -std=c++17 -g -Dsoyccan"
-    alias less="less --RAW-CONTROL-CHARS" # color only
-    alias ln="ln --symbolic"
-    alias ls="ls --human-readable --color=auto"
-    alias mv="mv --interactive"
-    alias rm="rm --interactive=always"
-    alias tar="tar --verbose"
-    alias wget="wget --timestamping --continue"
-    alias objdump="objdump -M intel -d"
-    alias uex='rm -rf ~/.idm/uex/ ~/.idm/*.spl /tmp/*.spl ; /usr/bin/uex'
-    alias hd='hexdump -C'
+alias gcc='gcc -Wall -Wextra -Wconversion -std=c18   -g -Dsoyccan'
+alias g++='g++ -Wall -Wextra -Wconversion -std=c++17 -g -Dsoyccan'
 
-    _docker_alias
-
-elif [ "$(uname)" = 'FreeBSD' ] || [ "$(uname)" = 'Darwin' ]; then
-    alias cp="cp -irv"
-    alias df="df -h"
-    alias du="du -h" # -A connot be used in macOS
-    alias free="free -h"
-    alias gcc="gcc -Wall -Wextra -Wconversion -std=c18   -g -Dsoyccan"
-    alias g++="g++ -Wall -Wextra -Wconversion -std=c++17 -g -Dsoyccan"
-    alias less="less -R" # color only
-    alias ln="ln -s"
-    alias ls="ls -hG"
-    alias mv="mv -i"
-    alias rm="rm -i"
-    alias tar="tar -v"
-    alias wget="wget --timestamping -c"
-    alias objdump="objdump -disassemble -x86-asm-syntax=intel"
-    alias uex='rm -rf ~/.idm/uex/ ~/.idm/*.spl /tmp/*.spl ; /usr/bin/uex'
-    alias hd='hexdump -C'
-
-    _docker_alias
+if [ "$(uname)" = 'Darwin' ]; then
+    alias objdump='objdump -x86-asm-syntax=intel'
+else
+    alias objdump='objdump -M intel'
 fi
+
+# docker
+alias dat='docker attach'
+alias dcon='docker container'
+alias dim='docker image'
+alias dps='docker ps'
+alias dpsa='docker ps -a'
+alias dsta='docker start -ai'
+alias dsto='docker stop'
+alias dex='docker exec -it'
