18efb6f58f27ce70edf9b9e327ae9b602bd14bba
soyccan <soyccan@gmail.com>
2023-11-24 11:47:48 +0800

    .

---
+++ .config/zsh/venv.zsh
@@ -0,0 +1,26 @@
+# Node.js Version Manager (NVM)
+if [[ -d "$HOME/.config/nvm" ]]; then
+    export NVM_DIR="${XDG_CONFIG_HOME:-$HOME/.config}nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+fi
+
+# Rust
+if [[ -e "$HOME/.cargo/env" ]]; then
+    source "$HOME/.cargo/env"
+fi
+
+# Pyenv
+if [[ -d "$HOME/.pyenv" ]]; then
+    export PYENV_ROOT="$HOME/.pyenv"
+    command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+fi
+
+# AWS CLI
+# if zshrc_has aws && zshrc_has aws_completer; then
+#     autoload bashcompinit && bashcompinit
+#     autoload -Uz compinit && compinit
+#     complete -C $(which aws_completer) aws # TODO: this does not work
+# fi
+
