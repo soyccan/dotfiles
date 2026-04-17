58d4f3783dc774013d61c5d228a1fece9bf5808a
Zac Chen <zacc@nvidia.com>
2026-04-17 08:55:33 +0800

    apt

---
+++ .config/fish/conf.d/apt.fish
@@ -0,0 +1,10 @@
+if not status is-interactive
+    exit
+end
+
+abbr --add aptf 'apt search'
+abbr --add apti 'sudo apt install -y'
+abbr --add apts 'apt show'
+abbr --add aptu 'sudo apt update'
+abbr --add aptU 'sudo apt upgrade'
+abbr --add aptx 'sudo apt autoremove --purge'
