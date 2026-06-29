e7333cec7695308086ad308249af0e0cc8b7a46c
Zac Chengkang Chen <soyccan@gmail.com>
2026-06-29 11:39:52 +0800

    vim: shellcehck

--- .config/nvim.kickstart/init.lua
+++ .config/nvim.kickstart/init.lua
@@ -797,6 +797,11 @@ do
     -- But for many setups, the LSP (`ts_ls`) will work just fine
     -- ts_ls = {},
 
+    -- shellcheck LSP
+    -- sudo apt install -y shellcheck npm
+    -- sudo npm install -g bash-language-server
+    bashls = {},
+
     stylua = {}, -- Used to format Lua code
 
     -- Special Lua Config, as recommended by neovim help docs
