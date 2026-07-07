bb7f9cd558d89b9c99b6c499d3bbc9a1c6711090
soyccan <soyccan@gmail.com>
2026-07-07 11:56:28 +0800

    fix zoxide in fish 4.8
    fish 4.8 moves cd into executable.
    There is no .../share/fish/functions/cd.fish

--- .config/fish/conf.d/zoxide.fish
+++ .config/fish/conf.d/zoxide.fish
@@ -16,7 +16,11 @@ end
 # A copy of fish's internal cd function. This makes it possible to use
 # `alias cd=z` without causing an infinite loop.
 if ! builtin functions --query __zoxide_cd_internal
-    string replace --regex -- '^function cd\s' 'function __zoxide_cd_internal ' <$__fish_data_dir/functions/cd.fish | source
+    if status list-files functions/cd.fish &>/dev/null
+        status get-file functions/cd.fish | string replace --regex -- '^function cd\s' 'function __zoxide_cd_internal ' | source
+    else
+        string replace --regex -- '^function cd\s' 'function __zoxide_cd_internal ' <$__fish_data_dir/functions/cd.fish | source
+    end
 end
 
 # cd + custom logic based on the value of _ZO_ECHO.
@@ -35,7 +39,7 @@ end
 #
 
 # Initialize hook to add new entries to the database.
-function __zoxide_hook_pwd --on-variable PWD
+function __zoxide_hook --on-variable PWD
     test -z "$fish_private_mode"
     and command zoxide add -- (__zoxide_pwd)
 end
@@ -118,9 +122,11 @@ end
 #
 
 abbr --erase z &>/dev/null
+complete --erase --command z
 alias z=__zoxide_z
 
 abbr --erase zi &>/dev/null
+complete --erase --command zi
 alias zi=__zoxide_zi
 
 # =============================================================================
